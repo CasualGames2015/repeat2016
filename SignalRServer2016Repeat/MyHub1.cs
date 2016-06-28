a77e7f6bbc192c18677b2078679cc4b11d94b63d
ITSPaul <powell.paul@itsligo.ie>
2016-06-28 14:30:14 +0100

    initial commit

---
+++ SignalRServer2016Repeat/MyHub1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRServer2016Repeat
+{
+    public class Player
+
+    {
+        public string PlayerID;
+        // Add more fields here
+    }
+
+    public static class HubState
+
+    {
+        public static List<Player> players = new List<Player>()
+
+        { new Player { PlayerID = "player1"  },
+         new Player { PlayerID = "player2"  },
+         // etc
+        };
+
+    }
+
+
+    public class MyHub1 : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public List<Player> getPlayers()
+        {
+            return HubState.players;
+        }
+    }
+}
---
+++ SignalrTestClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace SignalrTestClient
+{
+    public class Player
+
+    {
+        public string PlayerID;
+        // Add more fields here
+    }
+
+    class Program
+    {
+        static IHubProxy proxy;
+        static void Main(string[] args)
+        {
+            
+            HubConnection connection = new HubConnection("http://localhost:15250/");
+            proxy = connection.CreateHubProxy("MyHub1");
+            connection.Received += Connection_Received;
+            proxy.Invoke<List<Player>>("getPlayers", (Players) => {
+                foreach(Player p in Players)
+                {
+                    Console.WriteLine("Player ", p.PlayerID);
+                }
+            });
+
+            Console.ReadLine();
+        }
+
+        private static void Connection_Received(string obj)
+        {
+            Console.WriteLine("Message recieved to {0} ", obj);
+        }
+    }
+}
